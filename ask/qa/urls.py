edd86938875df986d931b298180592911008d6f6
Igor <prusovigor@gmail.com>
2017-01-20 20:58:57 +0000

    add ask django app

---
+++ ask/qa/urls.py
@@ -0,0 +1,12 @@
+from views import test
+
+
+urlpatterns = patterns('qa.views',
+    url(r'^$', test),
+    url(r'^login/$', test),
+    url(r'^signup/$', test),
+    url(r'^question/[0-9]+$', test),
+    url(r'^ask/$', test),
+    url(r'^popular/$', test),
+    url(r'^new/$', test),
+)
