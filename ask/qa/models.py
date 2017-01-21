0581ca99041b41a15950543b8ff1e86f35eec271
Igor Prusov <prusovigor@gmail.com>
2017-01-21 17:54:50 +0000

    Add models for qa app

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.OneToOneField(User)
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.OneToOneField(User)
