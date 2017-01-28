fb492eec65080bc6fc656f262ac5b26daa307cf2
Igor Prusov <prusovigor@gmail.com>
2017-01-28 20:02:16 +0000

    Make Question use custom manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,15 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Question(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
-
-
 class QuestionManager(models.Manager):
     def new(self):
         result = []
@@ -23,6 +14,16 @@ class QuestionManager(models.Manager):
         for question in Questions.objects.order_by('rating'):
             result.append(question)
         return result
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
+
 
 class Answer(models.Model):
     text = models.TextField()
