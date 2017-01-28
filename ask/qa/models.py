a68fd4225193de7112c5fe4a53d671bf780f38cf
Igor Prusov <prusovigor@gmail.com>
2017-01-28 19:58:56 +0000

    Fix fields properties

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
 
-class QuestionManger(models.Manager):
+class QuestionManager(models.Manager):
     def new(self):
         result = []
         for question in Questions.objects.latest('added_at'):
