2d9b61d59f1554212329d361c1c26c846d23744e
Igor Prusov <prusovigor@gmail.com>
2017-01-28 19:19:47 +0000

    Add related name to likes field

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     added_at = models.DateField()
     rating = models.IntegerField()
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
 
 
 class QuestionManger(models.Manager):
